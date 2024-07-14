06e3d332d0d36846292d6a2da931d4d91d7476ba
S-haw-n <shawn.kuria@strathmore.edu>
2024-07-14 13:31:28 +0300

    ...

--- tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-JButton button = new JButton(text);
-            button.setPreferredSize(new Dimension(150, (int) (getHeight() * heightRatio))); // Specific button height
-            button.setFont(label.getFont().deriveFont(Font.PLAIN, (float) (getHeight() * heightRatio * 0.3)));
-            button.addActionListener(e -> {
